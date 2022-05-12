42de0c5a593736310a0761caa5e4077c7a800c84
andrewgr1234 <85031521+andrewgr1234@users.noreply.github.com>
2022-05-12 22:12:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+var audio = new Audio("./elements/alarm.wav");
+var totalAmount = localStorage.getItem('countDown') || 0,
+    timeloop;
+
+if (totalAmount) {
+    timeSet()
+}
+
+function timeSet() {
+    totalAmount--;
+
+    localStorage.setItem('countDown', totalAmount);
+
+    if (totalAmount < 0) {
+        audio.play();
+        localStorage.removeItem('countDown');
+        totalAmount = 0;
+        clearTimeout(timeloop);
+        return;
+    }
+
+    var minutes = parseInt(totalAmount / 60);
+    var seconds = parseInt(totalAmount % 60);
+
+    if (seconds < 10)
+        seconds = "0" + seconds;
+
+    $('#tminus').val(minutes + ":" + seconds);
+
+    timeloop = setTimeout(timeSet, 1000);
+}
+
+$('.enterTime').click(function () {
+    var reqVal = $('#request').val();
+    var parAmt = parseInt(reqVal);
+    console.log($('#request'));
+    if (timeloop) {
+        clearTimeout(timeloop)
+    }
+    console.log(reqVal);
+    totalAmount = parAmt * 60;
+
+    $('#request').val(" ");
+
+    timeSet();
+})
+
+
+$('#countdown').on('reset', function () {
+    totalAmount = 0;
+    clearTimeout(timeloop);
+    localStorage.removeItem('countDown');
+})
